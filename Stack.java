e0e75f3b988e1a97b08fd869e9ae373e8a1cd154
Ninja-Jambon <lulul7018@gmail.com>
2024-02-21 11:48:49 +0100

    added a Stack class

---
+++ Stack.java
@@ -0,0 +1,31 @@
+// Antoine CRETUAL, Lukian LEIZOUR, 21/02/2024
+
+import java.util.Vector;
+
+public class Stack <T> {
+	// Atributes
+
+	private Vector <T> array; 
+
+	// Construcors
+
+	public Stack() {
+		array = new Vector<T>();
+	}
+
+	// Methods
+
+	public void push(T x) {
+		this.array.add(this.array.size(), x);
+	}
+
+	public T pop() {
+		T x = this.array.elementAt(this.array.size() - 1);
+		this.array.remove(this.array.size() - 1);
+		return x;
+	}
+
+	public int size() {
+		return this.array.size();
+	}
+}
